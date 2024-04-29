76eefb824e1f0b879eb7de7a6251052348a2d34a
Bhavanvir Rai <brai@uvic.ca>
2024-04-29 22:35:49 +0000

    Made method more generic

--- spreadsheetMapping/spreadsheetMapping/models.py
+++ spreadsheetMapping/spreadsheetMapping/models.py
@@ -1,20 +1,5 @@
 from django.db.models import Model, CharField
-
 class Contact(Model):
     name = CharField(max_length=255)
     number = CharField(max_length=20)
     address = CharField(max_length=255)
-
-    class SpreadsheetMeta:
-        columns = {
-            'name': 'A',
-            'number': 'B',
-            'address': 'C'
-        }
-
-    class Meta:
-        verbose_name = "Contact"
-        verbose_name_plural = "Contacts"
-
-    def __str__(self):
-        return self.name
