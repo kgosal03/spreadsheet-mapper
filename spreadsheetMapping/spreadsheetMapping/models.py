c950a4cb6f24a2499fa445ccc0eaadd356eed943
Bhavanvir Rai <brai@uvic.ca>
2024-04-30 16:32:33 +0000

    Now able to ingest a Sites sheet

--- spreadsheetMapping/spreadsheetMapping/models.py
+++ spreadsheetMapping/spreadsheetMapping/models.py
@@ -1,5 +1,11 @@
-from django.db.models import Model, CharField
-class Contact(Model):
-    name = CharField(max_length=255)
-    number = CharField(max_length=20)
-    address = CharField(max_length=255)
+from django.db.models import Model, FloatField, TextField, CharField, IntegerField
+class Sites(Model):
+    latitude = FloatField()
+    longitude = FloatField()
+    region = CharField(max_length=32, blank=True, null=True)
+    name = TextField()
+    code = CharField(max_length=32, unique=True, db_index=True)
+    c_dates = TextField(blank=True, null=True)
+    card_analogue = IntegerField(blank=True, null=True)
+    dating_notes = TextField(blank=True, null=True)
+    dating_ref = TextField(blank=True, null=True)
